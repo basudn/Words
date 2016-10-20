47f0064f7532c0ecf2ec94e05ea00f005eac8984
Debayan Basu <Debayan Basu@DESKTOP-EUF84R9>
2016-10-20 10:36:21 -0400

    Add project files.

---
+++ Words/Intro.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Words
+{
+    partial class Intro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.PlyBtn = new System.Windows.Forms.Button();
+            this.LevelLbl = new System.Windows.Forms.Label();
+            this.LevelSelect = new System.Windows.Forms.ComboBox();
+            this.MainLbl = new System.Windows.Forms.Label();
+            this.TimeKeeper = new System.Windows.Forms.Timer(this.components);
+            this.TimerLabel = new System.Windows.Forms.Label();
+            this.GuessLbl = new System.Windows.Forms.Label();
+            this.PuzzlePic = new System.Windows.Forms.PictureBox();
+            this.HangPic = new System.Windows.Forms.PictureBox();
+            this.MainPic = new System.Windows.Forms.PictureBox();
+            this.Score = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.PuzzlePic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.HangPic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MainPic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PlyBtn
+            // 
+            this.PlyBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PlyBtn.Location = new System.Drawing.Point(687, 431);
+            this.PlyBtn.Name = "PlyBtn";
+            this.PlyBtn.Size = new System.Drawing.Size(100, 50);
+            this.PlyBtn.TabIndex = 0;
+            this.PlyBtn.Text = "Play";
+            this.PlyBtn.UseVisualStyleBackColor = true;
+            this.PlyBtn.Click += new System.EventHandler(this.PlyBtn_Click);
+            // 
+            // LevelLbl
+            // 
+            this.LevelLbl.AutoSize = true;
+            this.LevelLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LevelLbl.Location = new System.Drawing.Point(570, 349);
+            this.LevelLbl.Name = "LevelLbl";
+            this.LevelLbl.Size = new System.Drawing.Size(114, 46);
+            this.LevelLbl.TabIndex = 3;
+            this.LevelLbl.Text = "Level";
+            // 
+            // LevelSelect
+            // 
+            this.LevelSelect.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LevelSelect.FormattingEnabled = true;
+            this.LevelSelect.Location = new System.Drawing.Point(745, 346);
+            this.LevelSelect.Name = "LevelSelect";
+            this.LevelSelect.Size = new System.Drawing.Size(150, 54);
+            this.LevelSelect.TabIndex = 5;
+            // 
+            // MainLbl
+            // 
+            this.MainLbl.AutoSize = true;
+            this.MainLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainLbl.Location = new System.Drawing.Point(584, 211);
+            this.MainLbl.Name = "MainLbl";
+            this.MainLbl.Size = new System.Drawing.Size(311, 73);
+            this.MainLbl.TabIndex = 7;
+            this.MainLbl.Text = "Hangman";
+            // 
+            // TimeKeeper
+            // 
+            this.TimeKeeper.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // TimerLabel
+            // 
+            this.TimerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TimerLabel.Location = new System.Drawing.Point(800, 75);
+            this.TimerLabel.Name = "TimerLabel";
+            this.TimerLabel.Size = new System.Drawing.Size(100, 40);
+            this.TimerLabel.TabIndex = 9;
+            this.TimerLabel.Text = "Timer";
+            this.TimerLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.TimerLabel.Visible = false;
+            // 
+            // GuessLbl
+            // 
+            this.GuessLbl.AutoSize = true;
+            this.GuessLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GuessLbl.Location = new System.Drawing.Point(332, 435);
+            this.GuessLbl.Name = "GuessLbl";
+            this.GuessLbl.Size = new System.Drawing.Size(135, 46);
+            this.GuessLbl.TabIndex = 11;
+            this.GuessLbl.Text = "Guess";
+            this.GuessLbl.Visible = false;
+            // 
+            // PuzzlePic
+            // 
+            this.PuzzlePic.Image = global::Words.Properties.Resources.Hang1;
+            this.PuzzlePic.Location = new System.Drawing.Point(340, 100);
+            this.PuzzlePic.Name = "PuzzlePic";
+            this.PuzzlePic.Size = new System.Drawing.Size(288, 300);
+            this.PuzzlePic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PuzzlePic.TabIndex = 10;
+            this.PuzzlePic.TabStop = false;
+            this.PuzzlePic.Visible = false;
+            // 
+            // HangPic
+            // 
+            this.HangPic.Image = global::Words.Properties.Resources.Hang1;
+            this.HangPic.Location = new System.Drawing.Point(774, 134);
+            this.HangPic.Name = "HangPic";
+            this.HangPic.Size = new System.Drawing.Size(144, 150);
+            this.HangPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.HangPic.TabIndex = 8;
+            this.HangPic.TabStop = false;
+            this.HangPic.Visible = false;
+            // 
+            // MainPic
+            // 
+            this.MainPic.Image = global::Words.Properties.Resources.Hang9;
+            this.MainPic.Location = new System.Drawing.Point(117, 124);
+            this.MainPic.Name = "MainPic";
+            this.MainPic.Size = new System.Drawing.Size(254, 271);
+            this.MainPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.MainPic.TabIndex = 6;
+            this.MainPic.TabStop = false;
+            // 
+            // Score
+            // 
+            this.Score.AutoSize = true;
+            this.Score.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Score.Location = new System.Drawing.Point(61, 75);
+            this.Score.Name = "Score";
+            this.Score.Size = new System.Drawing.Size(100, 37);
+            this.Score.TabIndex = 12;
+            this.Score.Text = "Score";
+            this.Score.Visible = false;
+            // 
+            // Intro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(978, 694);
+            this.Controls.Add(this.Score);
+            this.Controls.Add(this.GuessLbl);
+            this.Controls.Add(this.PuzzlePic);
+            this.Controls.Add(this.TimerLabel);
+            this.Controls.Add(this.HangPic);
+            this.Controls.Add(this.MainLbl);
+            this.Controls.Add(this.MainPic);
+            this.Controls.Add(this.LevelSelect);
+            this.Controls.Add(this.LevelLbl);
+            this.Controls.Add(this.PlyBtn);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Intro";
+            this.Text = "Hangman";
+            ((System.ComponentModel.ISupportInitialize)(this.PuzzlePic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.HangPic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MainPic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button PlyBtn;
+        private System.Windows.Forms.Label LevelLbl;
+        private System.Windows.Forms.ComboBox LevelSelect;
+        private System.Windows.Forms.PictureBox MainPic;
+        private System.Windows.Forms.Label MainLbl;
+        private System.Windows.Forms.PictureBox HangPic;
+        private System.Windows.Forms.Timer TimeKeeper;
+        private System.Windows.Forms.Label TimerLabel;
+        private System.Windows.Forms.PictureBox PuzzlePic;
+        private System.Windows.Forms.Label GuessLbl;
+        private System.Windows.Forms.Label Score;
+    }
+}
+
---
+++ Words/Intro.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Words
+{
+    public partial class Intro : Form
+    {
+        private string Word = "MANGO";
+        private int IncorrectGuesses = 0;
+        private int Timer;
+        private int SeqNo = 0;
+        private int TotalScore = 0;
+        private Bitmap[] hangImages = new Bitmap[] { Words.Properties.Resources.Hang1, Words.Properties.Resources.Hang2,
+                                        Words.Properties.Resources.Hang3, Words.Properties.Resources.Hang4,
+                                        Words.Properties.Resources.Hang5, Words.Properties.Resources.Hang6,
+                                        Words.Properties.Resources.Hang7};
+        private List<Control> ControlList = new List<Control>();
+        private List<char> Guesses = new List<char>();
+
+        public Intro()
+        {
+            InitializeComponent();
+        }
+
+        private void PlyBtn_Click(object sender, EventArgs e)
+        {
+            IncorrectGuesses = 0;
+            GenerateButtons();
+            LoadWordGuess();
+            GuessLbl.Show();
+            HideElements(new Control[] { MainPic, PlyBtn, MainLbl, LevelLbl, LevelSelect });
+            ShowElements(new Control[] { HangPic, PuzzlePic });
+            TimerLabel.Text = "0:0";
+            TimerLabel.Show();
+            Score.Text = "Score: " + TotalScore;
+            Score.Show();
+            TimeKeeper.Enabled = true;
+        }
+
+        private void LoadWordGuess()
+        {
+            GuessLbl.Text = "";
+            bool complete = true;
+            foreach (char letter in Word)
+            {
+                if (Guesses.Contains(letter))
+                {
+                    GuessLbl.Text += letter;
+                }
+                else
+                {
+                    GuessLbl.Text += '_';
+                    complete = false;
+                }
+                GuessLbl.Text += ' ';
+            }
+            if (complete)
+            {
+                SeqNo++;
+                TotalScore += SeqNo <= 4 ? 5 : SeqNo <= 8 ? 10 : 20;
+                Score.Text = "Score: " + TotalScore;
+                if (SeqNo == 10)
+                {
+                    TimeKeeper.Stop();
+                }
+                else
+                {
+                    RemoveControls(ControlList);
+                    GenerateButtons();
+                    Guesses = new List<char>();
+                    LoadWordGuess();
+                }
+            }
+        }
+
+        private void GenerateButtons()
+        {
+            int top = 365;
+            int left = 100;
+            int width = 35;
+            int height = 35;
+            for (char alpha = 'A'; alpha <= 'Z'; alpha++)
+            {
+                if (alpha == 'N')
+                {
+                    top = 400;
+                    left = 100;
+                }
+                Button button = new Button();
+                button.Name = "button" + alpha;
+                button.Text = alpha + "";
+                button.Top = top;
+                button.Left = left;
+                button.Width = width;
+                button.Height = height;
+                button.FlatStyle = FlatStyle.Flat;
+                button.Click += new EventHandler(GuessClick);
+                button.KeyDown += new KeyEventHandler(GuessKeyPress);
+                ControlList.Add(button);
+                this.Controls.Add(button);
+                button.Focus();
+                left += width;
+            }
+            Timer = 0;
+        }
+
+        private void RemoveControls(List<Control> controls)
+        {
+            foreach(Control control in controls)
+            {
+                this.Controls.Remove(control);
+            }
+            controls = new List<Control>();
+        }
+
+        private void HideElements(Control[] keys)
+        {
+            foreach (Control key in keys)
+            {
+                key.Hide();
+            }
+        }
+
+        private void ShowElements(Control[] keys)
+        {
+            foreach (Control key in keys)
+            {
+                key.Show();
+            }
+        }
+
+        private void GuessClick(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+            if (!Guesses.Contains(button.Text[0]))
+            {
+                button.BackColor = Color.Red;
+                button.Enabled = false;
+                Guesses.Add(button.Text[0]);
+                LoadWordGuess();
+                if (!Word.Contains(button.Text[0]))
+                {
+                    IncorrectGuesses++;
+                    if(IncorrectGuesses > 6)
+                    {
+                        TimeKeeper.Stop();
+                        TimerLabel.Font = new Font(FontFamily.GenericSansSerif, 20, FontStyle.Strikeout);
+                    }
+                    else
+                    {
+                        HangPic.Image = hangImages[IncorrectGuesses];
+                    }
+                }
+                Timer = 0;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            TimerLabel.Text = Timer / 10 + ":" + Timer % 10;
+            Timer++;
+            if(Timer == 100)
+            {
+                IncorrectGuesses++;
+                if (IncorrectGuesses > 6)
+                {
+                    TimeKeeper.Stop();
+                    TimerLabel.Font = new Font(FontFamily.GenericSansSerif, 20, FontStyle.Strikeout);
+                }
+                else
+                {
+                    HangPic.Image = hangImages[IncorrectGuesses];
+                }
+                Timer = 0;
+            }
+        }
+
+        private void GuessKeyPress(object sender, KeyEventArgs e)
+        {
+            if (ControlList.Count > 0 && e.KeyCode >= Keys.A && e.KeyCode <= Keys.Z)
+            {
+                Button keyPress = Controls.Find("button" + e.KeyCode, true)[0] as Button;
+                GuessClick(keyPress, new EventArgs());
+            }
+        }
+    }
+}
